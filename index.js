f7fb0a2251eabb485c28e91f166eb480def1ea44
Alex Wambugu <wambugumaigua@DESKTOP-F32N3MJ>
2024-03-21 05:11:07 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Write your solution here!
+describe('cats', function () {
+    var cats; 
+
+    beforeEach(function () {
+        cats = ["Milo", "Otis", "Garfield"]; 
+    });
+
+    it('is assigned an initial value of ["Milo", "Otis", "Garfield"]', function () {
+        expect(cats).toEqual(["Milo", "Otis", "Garfield"]);
+    });
+
+    describe('Array functions', function () {
+        it('destructivelyAppendCat(name)', function () {
+            cats.push("Milo", "Otis", "Garfield"); 
+
+            it('"before each" hook for "appends a cat to the end of the cats array"', function
+            });
+        });
+    });
+})
